d067d483dc851db8328c459bea3827a46f58fafd
dai.sun <dai.sun@yitu-inc.com>
2019-07-12 17:06:48 +0800

    build eureka

---
+++ keel-eureka/src/test/java/com/github/keel/eureka/KeelEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.keel.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeelEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
